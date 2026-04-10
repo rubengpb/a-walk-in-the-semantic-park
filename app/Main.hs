54d5005874438df430fed38fd95e8a2f4395ba21
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2026-04-10 22:19:52 +0200

    feat: term foldr, testing with nSymbols sum

--- app/Main.hs
+++ app/Main.hs
@@ -10,3 +10,5 @@ main :: IO ()
 main = do
     let formula = parseLogic "¬p | ((q |~ r))"
     print formula
+    let nSymbols = termFoldr (\_ -> 1) (\n -> n + 1) (\n m -> n + m + 3) (\n m -> n + m+ 3) formula
+    print nSymbols
--- app/Syntax.hs
+++ app/Syntax.hs
@@ -13,3 +13,18 @@ instance Show Term where
   show (Neg s) = "¬" ++ show s
   show (Conj t1 t2) = "(" ++ show t1 ++ " ∧ " ++ show t2 ++ ")"
   show (Disj t1 t2) = "(" ++ show t1 ++ " ∨ " ++ show t2 ++ ")"
+
+termFoldr :: (String -> a) -> (a -> a) -> (a -> a -> a) -> (a -> a -> a) -> Term -> a
+termFoldr var neg conj disj t =
+  let visit (Var x)      = var x
+      visit (Neg t')     = neg (visit t')
+      visit (Conj t1 t2) = conj (visit t1) (visit t2)
+      visit (Disj t1 t2) = disj (visit t1) (visit t2)
+  in visit t
+
+data TermNF =
+    PosVarNF String
+  | NegVarNF String
+  | CONJ_nf TermNF TermNF
+  | DISJ_nf TermNF TermNF
+  deriving (Eq, Show, Read)
